6880cc96fa465714caf05af3e79be36359c55c84
nbyrd9 <nianibyrd@gmail.com>
2021-05-03 09:01:32 -0400

    updates authors controller

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,2 +1,25 @@
 class AuthorsController < ApplicationController
+
+def index
+end
+
+def show
+end
+
+def create
+end
+
+def update
+end
+
+def destroy
+end
+
+
+
+private
+    def author_params
+    end
+
+
 end
